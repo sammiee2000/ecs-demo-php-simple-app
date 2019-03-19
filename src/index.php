e39eb1998f3c107a2acbfc6d73645371dc182094
sammiee2000 <35571465+sammiee2000@users.noreply.github.com>
2019-03-19 22:22:50 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,10 +15,8 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
+                <h1>Edureka DevOps Project</h1>
                 <h2>Congratulations</h2>
-                <p>Edureka LAB Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>Edureka LAB The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
